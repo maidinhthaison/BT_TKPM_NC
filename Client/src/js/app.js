df3eaa56c1e7aa996f28124dff2a09d2f9008386
maidinhthaison <admin@maidinhthaisons-MacBook-Pro.local>
2025-02-19 23:21:56 +0700

    end bt3

--- Client/src/js/app.js
+++ Client/src/js/app.js
@@ -4,8 +4,9 @@ const { apiClient } = require("./client");
 const { endpoint } = require("./endpoint");
 
 
-const traCuuNhanVien = async () => {
-  const url = endpoint.traCuuNv;
+const getNhanVien = async () => {
+  const url = endpoint.getNhanNv;
+  console.log(url);
   
   apiClient
     .get(url)
@@ -20,11 +21,45 @@ const traCuuNhanVien = async () => {
       console.error("Error:", error);
     });
 };
-const button = document.getElementById("btn");
-button.addEventListener("click",  async () => {
-  await traCuuNhanVien();
+window.addEventListener("load", async (event) => {
+  await getNhanVien();
 });
 
+
+const traCuuNhanVien = async (params) => {
+  const url = endpoint.traCuuNv;
+  
+  apiClient
+    .post(url, {
+         params,
+     })
+    .then((response) => {
+      const data = response.data;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+const form = document.getElementById("frm");
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  let cbDonVi = document.getElementById("cbDonVi");
+  let cbChiNhanh = document.getElementById("cbChiNhanh");
+  let donVi = cbDonVi.options[cbDonVi.selectedIndex].id;
+  let chiNhanh = cbChiNhanh.options[cbChiNhanh.selectedIndex].id;
+  let tukhoa = document.getElementById("tukhoa").value;
+  console.log(donVi);
+  console.log(chiNhanh);
+  console.log(tukhoa);
+  
+  const params = {
+    donVi: donVi,
+    chiNhanh: chiNhanh,
+    tukhoa: tukhoa
+  };
+  await traCuuNhanVien(params);
+});
 
 
 
--- Client/src/js/endpoint.js
+++ Client/src/js/endpoint.js
@@ -1,5 +1,6 @@
 let endpoint = {
   loichao: "/api/loichao",
+  getNhanNv: "/api/nhanvien",
   traCuuNv: "/api/nhanvien",
 };
 module.exports = { endpoint};
