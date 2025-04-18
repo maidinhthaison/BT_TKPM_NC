37b7d868b6b05b41e68c5e8528db1cf319165522
maidinhthaison <admin@maidinhthaisons-MacBook-Pro.local>
2025-04-18 22:28:26 +0700

    modify logic of dangNhapController

--- UserService/controller/userController.js
+++ UserService/controller/userController.js
@@ -1,19 +1,18 @@
 import { xuLyDangNhapService } from "../services/UserService.js";
 export const xuLyDangNhapController = async (req, res, next) => {
   const params = req.body.params;
+  if (!params) {
+    return res.status(400).send({ message: "Thiếu tham số" });
+  }
   const response = await xuLyDangNhapService(params.maSo, params.matKhau); 
-  try {
-    if(response.status === "OK"){
-      res.status(200).send(response);
-    }
-    else {
-      res.status(401).send(response);
-    }
-  } catch (error) {
-    console.log(res.status);
-    res.status(500).send(error.message);
-    
-  }    
+  console.log(`response: ${JSON.stringify(response, null, 2)}`);
+  
+  if(response.status === "OK"){
+    res.status(200).send(response);
+  }
+  else {
+    res.status(401).send(response);
+  }
 };
 
 // export const searchRoomsController = async (req, res, next) => {
