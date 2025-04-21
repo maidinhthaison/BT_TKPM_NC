950bce5cf9abcb4b544b98b7648658ba9d6076c6
maidinhthaison <admin@maidinhthaisons-MacBook-Pro.local>
2025-04-21 12:08:52 +0700

    modify models

--- OrderService/models/Order.js
+++ OrderService/models/Order.js
@@ -1,16 +1,12 @@
 import { KhachHang } from './KhachHang.js';
 import { Phong } from '../../RoomService/model/Phong.js'
 export class Order {
-    constructor(id, ngayThue, ngayTra, tongtien, khachHang, phong) {
+    constructor(id, ngayThue, ngayTra, tongtien, khachHangCccd, phongId) {
         this.id = id;
         this.ngayThue = ngayThue;
         this.ngayTra = ngayTra;
         this.tongtien = tongtien;
-        this.khachHang = new KhachHang(khachHang.cccd, khachHang.hoTen, khachHang.ngaySinh,
-            khachHang.diaChi, khachHang.dienThoai
-        );
-        this.phong = new Phong(phong.id, phong.tenPhong, phong.trangThai,
-            phong.loaiPhong, phong.khuVuc, phong.hinh
-        );
+        this.khachHangCccd = khachHangCccd;
+        this.phongId = phongId;
     }
 }
