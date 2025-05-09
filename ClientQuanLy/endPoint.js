192c948e9e0bf588cdd0cbfc65f8fb9a894dde78
maidinhthaison <admin@maidinhthaisons-MacBook-Pro.local>
2025-05-10 01:58:34 +0700

    them chuc nang tra cuu cho he khach quan ly

--- ClientQuanLy/endPoint.js
+++ ClientQuanLy/endPoint.js
@@ -1,5 +1,6 @@
 export const endPoint = {
-    loginEndPoint: "/api/user/login",
-    getStatisticByMonth : "/api/statistic/getStatisticByMonth"
-  };
-  
+  loginEndPoint: "/api/user/login",
+  getStatisticByMonth: "/api/statistic/getStatisticByMonth",
+  orderDetailEndPoint: "/api/order/getOrderDetails",
+  searchOrder: "/api/order/searchOrder",
+};
--- StatisticService/services/statisticService.js
+++ StatisticService/services/statisticService.js
@@ -90,13 +90,86 @@ export const getStatisticByMonthService = async (dateString) => {
   });
 };
 
-// function getMonthYearFromString(dateString) {
-//   const date = new Date(dateString);
-//   const monthIndex = date.getMonth();
-//   const year = date.getFullYear();
-//   const monthNumber = monthIndex + 1;
-//   return {
-//     month: monthNumber,
-//     year: year,
-//   };
-// }
+export const getStatisticByYearService = async (dateString) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const orders = await getAllOrders();
+
+      const rooms = await getAllRooms();
+
+      const khachHang = await getAllKhachHang();
+
+      const orderDetails = orders.orders.map((order) => {
+        const khachDatPhong = khachHang.khachHang.find(
+          (kh) => kh.cccd === order.khachHangCccd
+        );
+
+        const phongDaDat = rooms.listPhong.find((r) => r.id === order.phongId);
+
+        if (khachDatPhong !== undefined && phongDaDat !== undefined) {
+          return { ...order, khachDatPhong, phongDaDat };
+        }
+      });
+
+      const dateStr = getMonthYearFromString(dateString);
+
+      const result = [];
+      let totalIncome = 0;
+      let totalByMonth = 0;
+      orderDetails.forEach((element) => {
+        totalIncome += parseFloat(element.tongtien);
+        const date = getMonthYearFromString(element.ngayThue);
+        if (date.year === dateStr.year && date.month === dateStr.month) {
+          totalByMonth += parseFloat(element.tongtien);
+          result.push(element);
+        }
+      });
+
+      const grouped = result.reduce((acc, item) => {
+        const key = item.phongDaDat.loaiPhong.tenLoaiPhong;
+        if (!acc[key]) acc[key] = [];
+        acc[key].push(item);
+        return acc;
+      }, {});
+
+      let totalByCate = [];
+
+      for (const key in grouped) {
+        let item = grouped[key];
+        let totalIncomeByCate = 0;
+        item.forEach((element) => {
+          totalIncomeByCate += parseFloat(element.tongtien);
+        });
+
+        totalByCate.push({
+          type: key,
+          income: totalIncomeByCate,
+          tyle: parseFloat(
+            ((totalIncomeByCate / totalByMonth) * 100).toFixed(2)
+          ),
+        });
+      }
+      const summary = {
+        date: dateStr,
+        totalByMonth: totalByMonth,
+        totalIncome: totalIncome,
+        totalByCate: totalByCate,
+      };
+
+      resolve({
+        status: HTTP_CODE[200].code,
+        message: HTTP_CODE[200].message,
+        statistic: grouped,
+        summary: summary,
+      });
+    } catch (error) {
+      console.log(`getStatisticByMonthService 2222: ${error.message}`);
+      reject({
+        status: HTTP_CODE[500].code,
+        message: HTTP_CODE[500].message,
+      });
+    }
+  }).catch((error) => {
+    console.log(`getStatisticByMonthService 3333: ${error.message}`);
+  });
+};
